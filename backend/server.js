c6f5512efe1309d27f7ffc5b72f0cc6850e9e30a
AndreusSH <andreamammoliti@yahoo.it>
2024-05-11 15:18:40 +0200

    Starting a server built with express

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+//Entry point for our server
+import express from 'express'; //in json I set type: module to use this syntax
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const port = process.env.port || 5000
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => console.log(`Server started on route ${port}`));
