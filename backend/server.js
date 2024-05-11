acc2f590e08c21ac495481a7cecd864517cab12f
AndreusSH <andreamammoliti@yahoo.it>
2024-05-11 19:19:02 +0200

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express'; //in json I set type: module to use this syntax
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js'
 
 connectDB();
 
@@ -10,5 +11,7 @@ const port = process.env.port || 5000
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/users', userRoutes);
 
+app.get('/', (req, res) => res.send('Server is ready'));
 app.listen(port, () => console.log(`Server started on route ${port}`));
