02b06c35a9cf8bdaf90d9e93c252431e2ff07f71
AndreusSH <andreamammoliti@yahoo.it>
2024-05-11 19:16:59 +0200

    login, logout routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { createUser } from '../controllers/userControllers.js';
+import { createUser, loginUser, logoutUser } from '../controllers/userControllers.js';
 
 const router = express.Router();
 
 router.post("/", createUser);
+router.post('/login', loginUser);
+router.post('/logout', logoutUser);
 
 export default router;
