202f6d62358edab1296865077072794e0daa7d9f
AndreusSH <andreamammoliti@yahoo.it>
2024-05-11 15:35:55 +0200

    small mistake

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@
 import express from 'express'; //in json I set type: module to use this syntax
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 
+connectDB();
 
 const port = process.env.port || 5000
 const app = express();
