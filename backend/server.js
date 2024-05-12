415bd55352de01de6e82cc57a33aecfb66755612
AndreusSH <andreamammoliti@yahoo.it>
2024-05-12 11:02:37 +0200

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,24 @@
 //Entry point for our server
-import express from 'express'; //in json I set type: module to use this syntax
-import dotenv from 'dotenv';
-dotenv.config();
-import connectDB from './config/db.js';
+import express from 'express' //in json I set type: module to use this syntax
+import dotenv from 'dotenv'
+dotenv.config()
+import cookieParser from 'cookie-parser'
+const port = process.env.port || 5000
 import userRoutes from './routes/userRoutes.js'
+import connectDB from './config/db.js'
 
 connectDB();
 
-const port = process.env.port || 5000
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/api/users', userRoutes);
 
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cookieParser());
+app.use('/api/users', userRoutes);
 app.get('/', (req, res) => res.send('Server is ready'));
-app.listen(port, () => console.log(`Server started on route ${port}`));
+ 
+
+app.listen(port, () => console.log(`Server started on route ${port}`))
+
+ 
