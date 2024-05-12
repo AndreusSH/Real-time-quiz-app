beafef6c97a20d718070cdd6c8a288b1030c4f6e
AndreusSH <andreamammoliti@yahoo.it>
2024-05-12 10:55:36 +0200

    update and logout

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,44 +1,14 @@
-import User from '../models/userModels.js';
-import createToken from '../utils/createToken.js'
+import User from '../models/userModels.js'
+import generateToken from '../utils/generateToken.js'
 
-
-//route POST /api/users/
-const createUser = async (req, res) => {
-  const { name, email, password } = req.body;
-  const userExists = await User.findOne({ email })
-  if (userExists) {
-    res.status(400)
-    throw new Error('User already exists')
-  }
-  const user = await User.create({
-    name,
-    email,
-    password
-  })
-
-  if(user){
-    createToken(res, user._id);
-    res.status(201).json({
-        _id: user.id,
-        name: user.name,
-        password: user.password
-    })
-  }
-  else{
-    res.status(400);
-    throw new Error('Invalid User');
-  }
-
- 
-}
-
-//route POST /api/users/login
-const loginUser = async (req, res) => {
+//route POST /api/users/auth
+//@access Public
+const authUser = async (req, res) => {
   console.log(req.body)
   const { email, password } = req.body
   const user = await User.findOne({ email })
   if (user && (await user.matchPassword(password))) {
-    createToken(res, user._id)
+    generateToken(res, user._id)
     res.status(201).json({
       _id: user.id,
       name: user.name,
@@ -50,8 +20,38 @@ const loginUser = async (req, res) => {
   }
  }
 
- //route POST /api/users/logout
- const logoutUser = async (req, res) => {
+//route POST /api/users
+//@access Public
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if (user) {
+    generateToken(res, user._id)
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      password: user.password
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid User')
+  }
+ }
+
+//route POST /api/users/logout
+//@access Public
+const logoutUser = async (req, res) => {
   console.log(res)
   res.cookie('jwt', '', {
     httpOnly: true,
@@ -59,6 +59,40 @@ const loginUser = async (req, res) => {
   })
   res.status(200).json({ message: 'User logged out' })
 }
- 
 
-export { createUser, loginUser, logoutUser }
+//route GET /api/users/profile
+//@access Private
+const getUserProfile = async (req, res) => {
+  console.log(req.user)
+  const user = {
+    __id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  }
+  res.status(200).json(user)
+}
+
+//route PUT /api/users/profile
+//@access Private
+const updateUserProfile = async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (user) {
+    user.name = req.body.name || user.name
+    user.name = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updateUser = await user.save();
+    res.status(200).json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email
+    })
+  } else {
+    res.status(400)
+    throw new Error('User not found')
+  }
+ }
+
+export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile }
