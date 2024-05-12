1e768348e9d028971ee262c5db390f2b77b9740f
AndreusSH <andreamammoliti@yahoo.it>
2024-05-12 16:47:14 +0200

    use react-router-dom and different syntax

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import AdminPanel from '../views/AdminPanel'
+import { Profile } from '../views/Profile'
+import CreateQuizForm from '../views/CreateQuizForm'
 const App = () => {
   return (
-    <>
-      <h1>Mern Auth</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<AdminPanel />} />
+        <Route path='/api/users/profile' element={<Profile />} />
+        <Route path='/api/users/createQuiz' element={<CreateQuizForm />} />
+      </Routes>
+    </Router>
   )
 }
 
