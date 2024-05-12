2f6891851ee657807bda14c98678426ee66f4631
AndreusSH <andreamammoliti@yahoo.it>
2024-05-12 16:51:43 +0200

    create connection with the routes for quizzes

--- backend/server.js
+++ backend/server.js
@@ -7,24 +7,20 @@ dotenv.config()
 import cookieParser from 'cookie-parser'
 const port = process.env.port || 5000
 import userRoutes from './routes/userRoutes.js'
+import quizRoutes from './routes/quizRoutes.js'
 import connectDB from './config/db.js'
 
-connectDB();
-
-
-
+connectDB()
 
 const app = express()
-// Enable CORS for all requests  
-app.use(cors());
+// Enable CORS for all requests
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(cookieParser());
-app.use('/api/users', userRoutes);
-app.get('/', (req, res) => res.send('Server is ready'));
+app.use(cookieParser())
+app.use('/api/users', userRoutes)
+app.use('/api/quizzes', quizRoutes)
 
-
+app.get('/', (req, res) => res.send('Server is ready'))
 app.listen(port, () => console.log(`Server started on route ${port}`))
-
- 
