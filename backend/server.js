754d95a2c46325980409cf8ab51be48ecd31952b
AndreusSH <andreamammoliti@yahoo.it>
2024-05-13 15:54:15 +0200

    establish backend server connection

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,44 @@
-//Entry point for our server
-import express from 'express' //in json I set type: module to use this syntax
-import http from 'http'
-import socketio from 'socket.io'
-import cors from 'cors'
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
+import quizRoutes from './routes/quizRoutes.js';
 
-import dotenv from 'dotenv'
-dotenv.config()
-import cookieParser from 'cookie-parser'
-const port = process.env.port || 5000
-import userRoutes from './routes/userRoutes.js'
-import quizRoutes from './routes/quizRoutes.js'
-import connectDB from './config/db.js'
+dotenv.config();
+const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
-
-// This runs when the client connects
-io.on('connection', (socket) => {
-    console.log(`User is connected: ${socket.id}`)
-    console.log('New WS connection..');
-    socket.emit('message', 'Welcome to the Game')
-})
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server); // Initialize Socket.IO Server
+//console.log('io server is initialized', io)
 
 // Enable CORS for all requests
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cookieParser())
-app.use('/api/users', userRoutes)
-app.use('/api/quizzes', quizRoutes)
+app.use(cookieParser());
+app.use('/api/users', userRoutes);
+app.use('/api/quizzes', quizRoutes);
 
-app.get('/', (req, res) => res.send('Server is ready'))
-app.listen(port, () => console.log(`Server started on route ${port}`))
+app.get('/', (req, res) => res.send('Server is ready'));
+
+// Socket.IO connection handling
+io.on('connection', (socket) => {
+  console.log(`User connected Socket ID: ${socket.id}`);
+  console.log(socket.connected)  
+  socket.on('disconnect', () => {
+    console.log('user disconnected:', socket.id);
+  });
+});
 
 
-server.listen(3001, ()=> {
-    console.log('Server is running on 3001')
-})
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
