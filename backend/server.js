b69a73f31f4ace3e450eced2e8a3f1002e29ce98
AndreusSH <andreamammoliti@yahoo.it>
2024-05-13 11:37:05 +0200

    adding a socket connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 //Entry point for our server
 import express from 'express' //in json I set type: module to use this syntax
+import http from 'http'
+import socketio from 'socket.io'
 import cors from 'cors'
 
 import dotenv from 'dotenv'
@@ -13,6 +15,16 @@ import connectDB from './config/db.js'
 connectDB()
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+// This runs when the client connects
+io.on('connection', (socket) => {
+    console.log(`User is connected: ${socket.id}`)
+    console.log('New WS connection..');
+    socket.emit('message', 'Welcome to the Game')
+})
+
 // Enable CORS for all requests
 app.use(cors())
 app.use(express.json())
@@ -24,3 +36,8 @@ app.use('/api/quizzes', quizRoutes)
 
 app.get('/', (req, res) => res.send('Server is ready'))
 app.listen(port, () => console.log(`Server started on route ${port}`))
+
+
+server.listen(3001, ()=> {
+    console.log('Server is running on 3001')
+})
