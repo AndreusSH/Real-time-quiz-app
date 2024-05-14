4a21e295ae01df42bcaeb5c5f58c6c2eea98341c
AndreusSH <andreamammoliti@yahoo.it>
2024-05-14 12:41:15 +0200

    changing routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,11 @@ import Login from '../views/Login'
 import AdminPanel from '../views/AdminPanel'
 import Profile from '../views/Profile'
 import CreateQuizForm from '../views/CreateQuizForm'
+import PlayQuiz from '../views/PlayQuiz'
+//import QuizDetail from '../views/QuizDetail'
+
+
+//console.log('socket', socket)
 const App = () => {
   return (
     <Router>
@@ -11,6 +16,8 @@ const App = () => {
         <Route path='/api/users/profile' element={<Profile />} />
         <Route path='/api/users/admin' element={<AdminPanel />} />
         <Route path='/api/quizzes/createQuiz' element={<CreateQuizForm />} />
+        {/*<Route path="/quiz/:id" element={<QuizDetail />} /> */}
+        <Route path='/api/quizzes/playQuiz' element={<PlayQuiz />} />
       </Routes>
     </Router>
   )
