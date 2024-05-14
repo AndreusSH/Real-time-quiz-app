0388c45ce98f84bd91fa5d1a2ba74de587ceaca6
AndreusSH <andreamammoliti@yahoo.it>
2024-05-14 12:44:39 +0200

    logic to send the selected quiz to the server

--- frontend/views/Profile.jsx
+++ frontend/views/Profile.jsx
@@ -1,47 +1,56 @@
- 
- 
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import { Card } from 'react-bootstrap';
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { Card } from 'react-bootstrap'
+import io from 'socket.io-client'
+
+const socket = io.connect('http://localhost:8000/', {
+  transports: ['websocket', 'polling']
+})
 
 const Profile = () => {
-  const [quizzes, setQuizzes] = useState([]);
+  const [quizzes, setQuizzes] = useState([])
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/quizzes/allQuizzes')
+    axios
+      .get('http://localhost:8000/api/quizzes/allQuizzes')
       .then(response => {
-        setQuizzes(response.data);
+        setQuizzes(response.data)
       })
       .catch(error => {
-        console.error('Error fetching quizzes:', error);
-      });
-  }, []);
+        console.error('Error fetching quizzes:', error)
+      })
+  }, [])
+  const sendQuiz = quiz => {
+    socket.emit('selectedQuiz', quiz)
+  }
 
   return (
     <div>
       <h2>Profile</h2>
       <h3>Your Quizzes</h3>
-      <div className="row">
+      <div className='row'>
         {quizzes.map((quiz, index) => (
-          <div className="col-md-4 mb-4" key={index}>
-            <Card>
-              <Card.Body>
-                <Card.Title>Question: {quiz.question}</Card.Title>
-                <Card.Text>
-                  <ul>
-                    {quiz.answers.map((answer, i) => (
-                      <li key={i}>
-                        {answer.answer} - {answer.isCorrect ? 'Correct' : 'Incorrect'}
-                      </li>
-                    ))}
-                  </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
+          <div className='col-md-4 mb-4' key={index}>
+     
+              <Card>
+                <Card.Body onClick={() => sendQuiz(quiz)}>
+                  <Card.Title>Question: {quiz.question}</Card.Title>
+                  <Card.Text>
+                    <ul>
+                      {quiz.answers.map((answer, i) => (
+                        <li key={i}>
+                          {answer.answer} -{' '}
+                          {answer.isCorrect ? 'Correct' : 'Incorrect'}
+                        </li>
+                      ))}
+                    </ul>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+           </div>
         ))}
       </div>
     </div>
-  );
-};
-export default Profile;
+  )
+}
+export default Profile
