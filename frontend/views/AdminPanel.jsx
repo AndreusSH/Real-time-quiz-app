5b23794aaeb3bba7974a7f481cc9460e96a5ec1e
AndreusSH <andreamammoliti@yahoo.it>
2024-05-16 15:45:19 +0200

    admin panel made more modular

--- frontend/views/AdminPanel.jsx
+++ frontend/views/AdminPanel.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 import Profile from "./Profile";
+import TrackParticipants from "./TrackParticipants";
+ 
 
 export const AdminPanel = () => {
   return (
     <>
+         <TrackParticipants/>
          You can create a new quiz if you like: <Link to={'/api/quizzes/createQuiz'}>Add a quiz</Link>
+         {/*
+         se abbiamo un vincitore, facciamolo vedere a tutti!
+         */}
         <Profile/>
     </>
   )
