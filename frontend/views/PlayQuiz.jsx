9da59ade2895d22cc5961f4fdcba83a5384c88b3
AndreusSH <andreamammoliti@yahoo.it>
2024-05-16 16:06:30 +0200

    button styling and formatting

--- frontend/views/PlayQuiz.jsx
+++ frontend/views/PlayQuiz.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
+import { Button, Container, Row, Col, Alert } from 'react-bootstrap';
 import io from 'socket.io-client';
+import WinnerPage from './Winner';
 
 const socket = io.connect('http://localhost:8000/', {
   transports: ['websocket', 'polling']
@@ -9,43 +11,34 @@ const PlayQuiz = () => {
   const [quiz, setQuiz] = useState(null);
   const [timer, setTimer] = useState(0);
   const [isPaused, setIsPaused] = useState(false);
-  const [newTimer, setNewTimer] = useState(0); // Initialize newTimer state
-  const [winner, setWinner] = useState(false)
-  const [answer, setAnswer] = useState(null)
+  const [newTimer, setNewTimer] = useState(0);
+  const [winner, setWinner] = useState(false);
+  const [answer, setAnswer] = useState(null);
+
   useEffect(() => {
-    // Listen for "quizData" event from the server
     socket.on('quizData', quizData => {
-      console.log('Received quiz data:', quizData);
       setQuiz(quizData);
-      // Start the timer when quiz data is received
       setTimer(30); // Assuming the quiz duration is 30 seconds
-      setNewTimer(10); // Reset the new timer to 10 when quiz data is received
-      setIsPaused(false); // Ensure timer is not paused
+      setNewTimer(10);
+      setIsPaused(false);
     });
 
     socket.on('hello', message => {
-      console.log('Received pause notification:', message);
-      // Pause the timer when receiving pause notification
+      console.log(message)
       alert('Somebody booked to answer');
       setIsPaused(true);
       setQuiz(null);
-
     });
 
-    socket.on('newQuiz', prova => {
-      console.log('Received prova message:', prova);
+    socket.on('newQuiz', () => {
       setQuiz(null);
     });
 
-    socket.on('winner', winner => {
-      console.log('we got a winner and that is dope:', winner);
+    socket.on('winner', () => {
       setWinner(true);
       setQuiz(null);
     });
-    
-    
 
-    // Clean up event listeners on component unmount
     return () => {
       socket.off('quizData');
       socket.off('hello');
@@ -53,82 +46,83 @@ const PlayQuiz = () => {
     };
   }, []);
 
-  // Function to handle answer evaluation
   const evaluateAnswer = (answer) => {
-    if (answer.isCorrect) {
-      console.log('congrats this is the correct answer')
-      setAnswer(true)
-      // Emit score update to the server
-       socket.emit('scoreUpdate', { score: 1, timestamp: Date.now() });
+    if (isPaused) {
+      if (answer.isCorrect) {
+        setAnswer(true);
+        socket.emit('scoreUpdate', { score: 1, timestamp: Date.now() });
+      } else {
+        setAnswer(false);
+      }
+      setQuiz(null);
+      setIsPaused(true);
+    } else {
+      alert("You must stop the timer before answering");
     }
-    else{
-      console.log('this answer is absolutely not true')
-      setAnswer(false)
-
-    }
-
-    // Pause the timer when answering
-    setQuiz(null);
-    setIsPaused(true);
   };
 
-  // Function to handle timer tick
-  const handleTimerTick = () => { 
+  const handleTimerTick = () => {
     if (!isPaused && timer > 0) {
       setTimer(prevTimer => prevTimer - 1);
     }
 
-    // When the initial timer reaches 0, start the new countdown
     if (isPaused && newTimer > 0) {
       setNewTimer(prevTimer => prevTimer - 1);
     }
-  
   };
 
-  // Start the timer countdown
   useEffect(() => {
     const interval = setInterval(handleTimerTick, 1000);
-
-    // Clean up timer interval on component unmount or when timer reaches 0
     return () => {
       clearInterval(interval);
     };
-  }, [isPaused, timer, newTimer]); // Re-run the timer effect when isPaused, timer, or newTimer changes
+  }, [isPaused, timer, newTimer]);
 
-  // Function to handle timer stopping
   const stopTimer = () => {
     setIsPaused(true);
-    //setTimer(10); // Set the initial timer to 10 seconds
-    setNewTimer(10); // Reset the new timer to 10 seconds
+    setNewTimer(10);
     socket.emit('pauseNotification', { message: 'Somebody booked to answer' });
   };
+
   return (
     <>
       {quiz ? (
-        <div>
-          <h2>Quiz Result</h2>
-          <p>Question: {quiz.question}</p>
-          <p>Time left: {!isPaused ? timer : newTimer} seconds</p> {/* Display the current timer */}
-          <button onClick={stopTimer}>Stop Timer</button>
-          <ul>
-            {quiz.answers.map((answer, i) => (
-              <li key={i}>
-                <button onClick={() => evaluateAnswer(answer)}>
-                  {answer.answer} - {answer.isCorrect ? 'Correct' : 'Incorrect'}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col xs={12} md={8}>
+              <h2>Quiz Result</h2>
+              <p>Question: {quiz.question}</p>
+              <p>Time left: {!isPaused ? timer : newTimer} seconds</p>
+              <Button variant="danger" onClick={stopTimer} className="mr-3 mb-3">Stop Timer</Button>
+              <ul className="list-unstyled">
+                {quiz.answers.map((answer, i) => (
+                  <li key={i} className="mr-3 mb-3">
+                    <Button variant="outline-primary" onClick={() => evaluateAnswer(answer)}>
+                      {answer.answer} - {answer.isCorrect ? 'Correct' : 'Incorrect'}
+                    </Button>
+                  </li>
+                ))}
+              </ul>
+            </Col>
+          </Row>
+        </Container>
       ) : winner ? (
-        <p>Congrats you won</p>
+        <WinnerPage />
       ) : answer === null ? (
         <p>You will be notified when there is a new question</p>
       ) : (
-        <p>Your answer was {answer ? 'Correct' : 'Incorrect'} - You will be notified when there is a question available</p>
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col xs={12} md={8}>
+              <Alert variant={answer ? 'success' : 'danger'}>
+                Your answer was {answer ? 'Correct' : 'Incorrect'} - You will be notified when there is a question available
+              </Alert>
+            </Col>
+          </Row>
+        </Container>
       )}
     </>
   );
-}  
+};
 
 export default PlayQuiz;
