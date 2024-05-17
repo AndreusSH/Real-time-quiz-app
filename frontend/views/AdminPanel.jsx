7911d14869a395270960a2a116ffca88e5ce1eac
AndreusSH <andreamammoliti@yahoo.it>
2024-05-17 09:41:02 +0200

    minor changes

--- frontend/views/AdminPanel.jsx
+++ frontend/views/AdminPanel.jsx
@@ -1,18 +1,31 @@
-import { Link } from "react-router-dom";
-import Profile from "./Profile";
-import TrackParticipants from "./TrackParticipants";
- 
 
+import { Link } from 'react-router-dom'
+import Profile from './Profile'
+import TrackParticipants from './TrackParticipants'
+import { Fragment } from 'react'
+ import { useSelector } from 'react-redux'
+import { selectWinner } from '../src/features/winner/winnerSlice'
+
+ 
 export const AdminPanel = () => {
+  const winner = useSelector(selectWinner)
+  console.log('has a winner', winner)
+     
+  
   return (
     <>
-         <TrackParticipants/>
-         You can create a new quiz if you like: <Link to={'/api/quizzes/createQuiz'}>Add a quiz</Link>
-         {/*
-         se abbiamo un vincitore, facciamolo vedere a tutti!
-         */}
-        <Profile/>
+      <h1>Admin page</h1>
+      <Fragment>
+        Here you can create a new quiz or choose an existing one
+      </Fragment>
+      <div>
+        <TrackParticipants />
+      </div>
+      You can create a new quiz if you like:{' '}
+      <Link to={'/api/quizzes/createQuiz'}>Add a quiz</Link>
+      {winner ? (<h2>We have a winner</h2>) : (null)}
+      <Profile />
     </>
   )
 }
-export default AdminPanel;
+export default AdminPanel
